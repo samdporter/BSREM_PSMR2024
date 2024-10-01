52ad3af433beaefac1abfa9169b37eaa0bc16da1
Sam.Porter <porter.samd@gmail.com>
2024-10-01 11:11:13 +0100

    Removed TV (it's just TNV with zero weighting for other modality), flip SPET attn

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ import pandas as pd
 from numbers import Number
 import shutil
 
-# import gaussian filter
-from scipy.ndimage import gaussian_filter
-
 # SIRF imports
 from sirf.STIR import (ImageData, AcquisitionData, 
                        AcquisitionModelUsingMatrix, 
@@ -29,7 +26,6 @@ from cil.optimisation.operators import BlockOperator, ZeroOperator
 
 parser = argparse.ArgumentParser(description='BSREM')
 
-parser.add_argument('--modality', type=str, default='both', help='modality - can be pet, spect or both')
 parser.add_argument('--alpha', type=float, default=256, help='alpha')
 parser.add_argument('--beta', type=float, default=10, help='beta')
 parser.add_argument('--delta', type=float, default=1e-6, help='delta')
@@ -40,7 +36,7 @@ parser.add_argument('--iterations', type=int, default=240, help='max iterations'
 parser.add_argument('--update_interval', type=int, default=12, help='update interval')
 parser.add_argument('--relaxation_eta', type=float, default=0.1, help='relaxation eta')
 
-parser.add_argument('--data_path', type=str, default="/home/sam/data/anthropomorphic_Y90/phantom/for_cluster", help='data path')
+parser.add_argument('--data_path', type=str, default="/home/sam/data/phantom_data/for_cluster", help='data path')
 parser.add_argument('--output_path', type=str, default="/home/sam/working/BSREM_PSMR_MIC_2024/results/test", help='output path')
 parser.add_argument('--source_path', type=str, default='/home/sam/working/BSREM_PSMR_MIC_2024/src', help='source path')
 parser.add_argument('--working_path', type=str, default='/home/sam/working/BSREM_PSMR_MIC_2024/tmp', help='working path')
@@ -108,6 +104,10 @@ def get_spect_data(path):
     spect_data["acquisition_data"] = AcquisitionData(os.path.join(path,  "SPECT/peak_1_projdata__f1g1d0b0.hs"))
     spect_data["additive"] = AcquisitionData(os.path.join(path,  "SPECT/simind_scatter_ellipses_megp_cpd.hs"))
     spect_data["attenuation"] = ImageData(os.path.join(path,  "SPECT/umap_zoomed.hv"))
+    # Need to flip the attenuation image on the x-axis due to bug in STIR
+    attn_arr = spect_data["attenuation"].as_array()
+    attn_arr = np.flip(attn_arr, axis=-1)
+    spect_data["attenuation"].fill(attn_arr)
     spect_data["initial_image"] = ImageData(os.path.join(path,  "SPECT/spect_osem_20.hv")).maximum(0)
 
     return spect_data
@@ -125,12 +125,12 @@ def get_pet_am(pet_data, gpu):
         pet_am.set_num_tangential_LORs(10)
     asm = AcquisitionSensitivityModel(pet_data["normalisation"])
     pet_am.set_acquisition_sensitivity(asm)
-    pet_am.set_additive_term(pet_data["additive"]/50)
+    pet_am.set_additive_term(pet_data["additive"])
     # using adjoint(forard(image)) & STIR find_fwhm_in_image
     # 1cm FWHM from NEMA 2001 (Mediso AnyScan specificaitons)
     # operations applied one after the other to find total FWHM
     pet_psf = SeparableGaussianImageFilter()
-    pet_psf.set_fwhms((4.2,4.1,4.1)) 
+    pet_psf.set_fwhms((4.5,7.5,7.5)) 
     pet_am.set_image_data_processor(pet_psf)
     #pet_am.set_up(pet_data["acquisition_data"], pet_data["initial_image"])
     return pet_am
@@ -141,17 +141,15 @@ def get_spect_am(spect_data, keep_all_views_in_cache=False):
     spect_am_mat.set_keep_all_views_in_cache(keep_all_views_in_cache)
     # using Tc99m (140 keV) AnyScan measured resolution modelling
     # close enough to 150 keV Y90
-    #spect_mat.set_resolution_model(1.81534, 0.02148, False) 
-    # spect_mat.set_resolution_model(0.9323, 0.03, False) 
-    #spect_am = AcquisitionModelUsingMatrix(spect_mat)
-    #spect_psf = SeparableGaussianImageFilter()
+    # spect_am_mat.set_resolution_model(1.81534, 0.02148, False) 
+    spect_am_mat.set_resolution_model(0.9323, 0.03, False) 
     # using gaussians estimated from Y90 data (see notebook)
     # /home/sam/working/simulated_data/data/data.ipynb
-    #spect_psf.set_fwhms((6.61, 6.61, 6.61))
-    #spect_am.set_image_data_processor(spect_psf)
-    spect_am_mat.set_resolution_model(0.93,0.03, False) 
     spect_am = AcquisitionModelUsingMatrix(spect_am_mat)
-    spect_am.set_additive_term(spect_data["additive"]) #TODO: change back
+    spect_psf = SeparableGaussianImageFilter()
+    #spect_psf.set_fwhms((22, 19, 19))
+    #spect_am.set_image_data_processor(spect_psf)
+    #spect_am.set_additive_term(spect_data["additive"]) #TODO: change back
     #spect_am.set_up(spect_data["acquisition_data"], spect_data["initial_image"])
     return spect_am
 
@@ -177,13 +175,6 @@ def get_vectorial_tv(bo, ct, alpha, beta, initial_estimates, delta, gpu=False, k
     jac_co = CompositionOperator([bo, jac])
     jac_co.range_geometry = lambda: initial_estimates
     return OperatorCompositionFunction(vtv, jac_co)
-
-def get_tv(operator, ct, regularisation_parameter, initial_estimate, delta):
-    dtv = regularisation_parameter * FairL21Norm(delta=delta)
-    grad = NumpyDataContainer(operator.direct(initial_estimate), DirectionalGradient(anatomical=ct.as_array(), voxel_sizes=ct.voxel_sizes()))
-    grad_co = CompositionOperator([operator, grad])
-    grad_co.range_geometry = lambda: initial_estimate
-    return OperatorCompositionFunction(dtv, grad_co) 
 
 def compute_kappa_squared_image(obj_fun, initial_image):
     '''
@@ -228,95 +219,65 @@ def main(args):
     ct+=(-ct).max()
     ct/=ct.max()    
 
-    if args.modality.lower() == "pet" or args.modality.lower() == "both":
+    pet_data  = get_pet_data(args.data_path)
+    cyl.apply(pet_data["initial_image"])
+    #gauss.apply(pet_data["initial_image"])
+    pet_data["initial_image"].write("initial_image_0.hv")
 
-        pet_data  = get_pet_data(args.data_path)
-        cyl.apply(pet_data["initial_image"])
-        #gauss.apply(pet_data["initial_image"])
-        pet_data["initial_image"].write("initial_image_0.hv")
-
-        pet2ct = NiftyResampleOperator(ct, pet_data["initial_image"], AffineTransformation(os.path.join(args.data_path, "Registration", "pet_to_ct_smallFOV.txt")))
-        pet_am = get_pet_am(pet_data,  gpu=True)
-        pet_am.direct = lambda x: pet_am.forward(x)
-        pet_am.adjoint = lambda x: pet_am.backward(x)
-        
-        pet_obj_fun = get_objective_function(pet_data["acquisition_data"], pet_am, pet_data["initial_image"], pet_num_subsets)
-
-    if args.modality.lower() == "spect" or args.modality.lower() == "both":
-
-        spect_data  = get_spect_data(args.data_path)
-        cyl.apply(spect_data["initial_image"])
-        #gauss.apply(spect_data["initial_image"])
-        spect_data["initial_image"].write("initial_image_1.hv")
-
-        spect2ct = NiftyResampleOperator(ct, spect_data["initial_image"], AffineTransformation(os.path.join(args.data_path, "Registration", "spect_to_ct_smallFOV.txt")))
-        spect_am = get_spect_am(spect_data, args.keep_all_views_in_cache)
-        spect_am.direct = lambda x: spect_am.forward(x)
-        spect_am.adjoint = lambda x: spect_am.backward(x)
-        
-        spect_obj_fun = get_objective_function(spect_data["acquisition_data"], spect_am, spect_data["initial_image"], spect_num_subsets)
+    pet2ct = NiftyResampleOperator(ct, pet_data["initial_image"], AffineTransformation(os.path.join(args.data_path, "Registration", "pet_to_ct_smallFOV.txt")))
+    pet_am = get_pet_am(pet_data,  gpu=True)
+    pet_am.direct = lambda x: pet_am.forward(x)
+    pet_am.adjoint = lambda x: pet_am.backward(x)
     
-    if args.modality.lower() == "both":
-        zero_pet2ct = ZeroOperator(pet_data["initial_image"], ct)
-        zero_spect2ct = ZeroOperator(spect_data["initial_image"], ct)
+    pet_obj_fun = get_objective_function(pet_data["acquisition_data"], pet_am, pet_data["initial_image"], pet_num_subsets)
 
-        bo = BlockOperator(pet2ct, zero_spect2ct,
-                            zero_pet2ct, spect2ct, 
-                            shape = (2,2))
+    spect_data  = get_spect_data(args.data_path)
+    cyl.apply(spect_data["initial_image"])
+    #gauss.apply(spect_data["initial_image"])
+    spect_data["initial_image"].write("initial_image_1.hv")
 
-        initial_estimates = BlockDataContainer(pet_data["initial_image"], spect_data["initial_image"])
+    spect2ct = NiftyResampleOperator(ct, spect_data["initial_image"], AffineTransformation(os.path.join(args.data_path, "Registration", "spect_to_ct_smallFOV.txt")))
+    spect_am = get_spect_am(spect_data, args.keep_all_views_in_cache)
+    spect_am.direct = lambda x: spect_am.forward(x)
+    spect_am.adjoint = lambda x: spect_am.backward(x)
+    
+    spect_obj_fun = get_objective_function(spect_data["acquisition_data"], spect_am, spect_data["initial_image"], spect_num_subsets)
+    
+    zero_pet2ct = ZeroOperator(pet_data["initial_image"], ct)
+    zero_spect2ct = ZeroOperator(spect_data["initial_image"], ct)
 
-        kappa = bo.direct(BlockDataContainer(compute_kappa_squared_image(pet_obj_fun, pet_data["initial_image"]),compute_kappa_squared_image(spect_obj_fun, spect_data["initial_image"])))
-        for i, el in enumerate(kappa.containers):
-            gauss.apply(el)
-            el.write(f"kappa_{i}.hv")
-        #kappa = None
+    bo = BlockOperator(pet2ct, zero_spect2ct,
+                        zero_pet2ct, spect2ct, 
+                        shape = (2,2))
 
-        prior = get_vectorial_tv(bo, ct, args.alpha, args.beta, initial_estimates, delta=args.delta, gpu=args.gpu, kappa=kappa)
+    initial_estimates = BlockDataContainer(pet_data["initial_image"], spect_data["initial_image"])
 
-        pet2spect_zero = ZeroOperator(pet_data["initial_image"], spect_data["acquisition_data"])
-        spect2pet_zero = ZeroOperator(spect_data["initial_image"], pet_data["acquisition_data"])
+    kappa = bo.direct(BlockDataContainer(compute_kappa_squared_image(pet_obj_fun, pet_data["initial_image"]),compute_kappa_squared_image(spect_obj_fun, spect_data["initial_image"])))
+    for i, el in enumerate(kappa.containers):
+        gauss.apply(el)
+        el.write(f"kappa_{i}.hv")
+    #kappa = None
 
-        acquisition_model = BlockOperator(pet_am, spect2pet_zero,
-                                            pet2spect_zero, spect_am,
-                                        shape=(2,2)) 
-        data = BlockDataContainer(pet_data["acquisition_data"], spect_data["acquisition_data"])
+    prior = get_vectorial_tv(bo, ct, args.alpha, args.beta, initial_estimates, delta=args.delta, gpu=args.gpu, kappa=kappa)
 
-        initial = initial_estimates
-        
-    elif args.modality.lower() == "pet":
-        acquisition_model = pet_am
-        data = pet_data["acquisition_data"]
-        prior = get_tv(pet2ct, ct, args.alpha, pet_data["initial_image"], delta=args.delta)
-        obj_fun = pet_obj_fun
+    pet2spect_zero = ZeroOperator(pet_data["initial_image"], spect_data["acquisition_data"])
+    spect2pet_zero = ZeroOperator(spect_data["initial_image"], pet_data["acquisition_data"])
 
-        initial = pet_data["initial_image"]
+    acquisition_model = BlockOperator(pet_am, spect2pet_zero,
+                                        pet2spect_zero, spect_am,
+                                    shape=(2,2)) 
+    data = BlockDataContainer(pet_data["acquisition_data"], spect_data["acquisition_data"])
 
-    elif args.modality.lower() == "spect":
-        acquisition_model = spect_am
-        data = spect_data["acquisition_data"]
-        prior = get_tv(spect2ct, ct, args.beta, spect_data["initial_image"], delta=args.delta)
-        obj_fun = spect_obj_fun
-
-        initial = spect_data["initial_image"]
+    initial = initial_estimates
         
     acquisition_model.is_linear = MethodType(lambda self: True, acquisition_model)
         
-
-    if args.modality.lower() == "both":
-        bsrem=BSREMmm_of(SIRFBlockFunction([pet_obj_fun, spect_obj_fun]), prior, 
-                         initial=initial, initial_step_size=1, relaxation_eta=args.relaxation_eta, 
-                         update_objective_interval=args.update_interval, save_path=args.working_path,
-                         stochastic=args.stochastic, svrg=args.svrg, saga=args.saga, with_replacement=args.with_replacement,
-                         single_modality_update=args.single_modality_update, save_images = args.save_images,
-                         prior_is_subset=args.prior_is_subset, update_max=100*initial.max(), prior_in_precond=True)
-    else:
-        bsrem = BSREMmm_of(obj_fun, prior, initial=initial, initial_step_size=1, 
-                           relaxation_eta=args.relaxation_eta, 
-                           update_objective_interval=args.update_interval, save_path=args.working_path,
-                           stochastic=args.stochastic, svrg=args.svrg, saga=args.saga, with_replacement=args.with_replacement,
-                           single_modality_update=False, save_images=args.save_images,
-                           prior_is_subset=args.prior_is_subset, update_max=100*initial.max())
+    bsrem=BSREMmm_of(SIRFBlockFunction([pet_obj_fun, spect_obj_fun]), prior, 
+                        initial=initial, initial_step_size=100, relaxation_eta=args.relaxation_eta, 
+                        update_objective_interval=args.update_interval, save_path=args.working_path,
+                        stochastic=args.stochastic, svrg=args.svrg, saga=args.saga, with_replacement=args.with_replacement,
+                        single_modality_update=args.single_modality_update, save_images = args.save_images,
+                        prior_is_subset=args.prior_is_subset, update_max=100*initial.max(), prior_in_precond=True)
 
     bsrem.max_iteration=args.iterations
     bsrem.run(args.iterations, verbose=2)
@@ -324,30 +285,34 @@ def main(args):
     return bsrem
 
 if __name__ == "__main__":
-
-    _ = MessageRedirector()
     
-    # create dataframe of all args
+    # Redirect messages if needed
+    _ = MessageRedirector()
+
+    # Create a dataframe for all arguments and save as CSV
     df_args = pd.DataFrame([vars(args)])
     df_args.to_csv(os.path.join(args.output_path, "args.csv"))
 
-    # print all args
+    # Print all arguments
     for key, value in vars(args).items():
         print(f"{key}: {value}")
 
+    # Run main function and retrieve result
     bsrem = main(args)
 
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
+    # Ensure output path exists
+    os.makedirs(args.output_path, exist_ok=True)
 
+    # Save reconstructed images based on type
     if isinstance(bsrem.x, ImageData):
         bsrem.x.write(os.path.join(args.output_path, f"bsrem_a_{args.alpha}_b_{args.beta}.hv"))
     elif isinstance(bsrem.x, BlockDataContainer):
         for i, el in enumerate(bsrem.x.containers):
             el.write(os.path.join(args.output_path, f"bsrem_modality_{i}_a_{args.alpha}_b_{args.beta}.hv"))
-    
-    df = pd.DataFrame(l[0] for l in bsrem.loss) 
-    df.to_csv(os.path.join(args.output_path, f"bsrem_objective_a_{args.alpha}_b_{args.beta}.csv"))
+
+    # Save loss data
+    df_objective = pd.DataFrame([l[0] for l in bsrem.loss])
+    df_objective.to_csv(os.path.join(args.output_path, f"bsrem_objective_a_{args.alpha}_b_{args.beta}.csv"))
 
     df_data = pd.DataFrame([l[1] for l in bsrem.loss])
     df_prior = pd.DataFrame([l[2] for l in bsrem.loss])
@@ -355,21 +320,22 @@ if __name__ == "__main__":
     df_data.to_csv(os.path.join(args.output_path, f"bsrem_data_a_{args.alpha}_b_{args.beta}.csv"))
     df_prior.to_csv(os.path.join(args.output_path, f"bsrem_prior_a_{args.alpha}_b_{args.beta}.csv"))
 
-    # stick in one file
-    df_full = pd.concat([df, df_data, df_prior], axis=1)
-    # add column names
+    # Combine loss data into a single CSV
+    df_full = pd.concat([df_objective, df_data, df_prior], axis=1)
     df_full.columns = ["Objective", "Data", "Prior"]
     df_full.to_csv(os.path.join(args.output_path, f"bsrem_full_a_{args.alpha}_b_{args.beta}.csv"))
 
+    # Remove temporary files
     for file in os.listdir(args.working_path):
         if file.startswith("tmp_") and (file.endswith(".s") or file.endswith(".hs")):
-            os.remove(file)
+            os.remove(os.path.join(args.working_path, file))
 
-    # move any leftover files to output path
+    # Move leftover files (if any) to the output path
     for file in os.listdir(args.working_path):
-        if (file.endswith(".hv") or file.endswith(".v") or file.endswith(".ahv")):
+        if file.endswith((".hv", ".v", ".ahv")):
             print(f"Moving {file}")
             shutil.move(os.path.join(args.working_path, file), os.path.join(args.output_path, file))
 
     print("Done")
+
 
